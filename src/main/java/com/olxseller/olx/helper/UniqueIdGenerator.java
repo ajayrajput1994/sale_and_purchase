da89421415ba30a96f492911d31044f7ac0915eb
ajayrajput1994 <55807531+ajayrajput1994@users.noreply.github.com>
2025-08-13 00:15:13 +0530

    Update UniqueIdGenerator.java

--- src/main/java/com/olxseller/olx/helper/UniqueIdGenerator.java
+++ src/main/java/com/olxseller/olx/helper/UniqueIdGenerator.java
@@ -3,36 +3,34 @@ package com.olxseller.olx.helper;
 import java.util.Random;
 import java.util.UUID;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-public class UniqueIdGenerator { 
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final int CODE_LENGTH = 10; // Including the 'P'
-    @Bean
-    public String generateOrderId() {
-        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 12).toUpperCase();
-        return "ORD"+uuid;
-    }
-    @Bean
-    public  String generateProductId() {
-        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 10).toUpperCase();
-        System.out.println("genratepod: "+uuid);
-        return "P"+uuid;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniqueIdGenerator {
+
+  private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  private static final int CODE_LENGTH = 10;
+
+  public String generateOrderId() {
+    String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 12).toUpperCase();
+    return "ORD" + uuid;
+  }
+
+  public String generateProductId() {
+    String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 10).toUpperCase();
+    System.out.println("Generated Product ID: " + uuid);
+    return "P" + uuid;
+  }
+
+  public String generateUniqueProductCode() {
+    StringBuilder productId = new StringBuilder("P");
+    Random random = new Random();
+
+    for (int i = 0; i < CODE_LENGTH; i++) {
+      int index = random.nextInt(CHARACTERS.length());
+      productId.append(CHARACTERS.charAt(index));
     }
 
-    @Bean
-    public String generateUniqueProductCode() {
-        // Start with 'P'
-        StringBuilder productId = new StringBuilder("P");
-        Random random = new Random();
-
-        // Generate remaining characters
-        for (int i = 0; i < CODE_LENGTH; i++) {
-            int index = random.nextInt(CHARACTERS.length());
-            productId.append(CHARACTERS.charAt(index));
-        }
-
-        return productId.toString();
-    }
+    return productId.toString();
+  }
 }
